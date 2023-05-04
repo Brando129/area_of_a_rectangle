bf1166d9cf2a5bce250b13fec3669af0cf2eaebd
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-04 10:17:26 -0400

    Initial Commit

---
+++ index.py
@@ -0,0 +1,10 @@
+# Create a function that is able to tell a user what the area of a rectangle is
+
+def find_area(length, width):
+    length = length
+    width = width
+    area = length * width
+
+    return f"The area of your rectangle is {area}"
+
+print(find_area(20, 8))
